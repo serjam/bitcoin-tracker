a478b56aec9e35a5e22f97446fb2da326ca09e1d
jnsr <xnasser@outlook.com>
2018-07-10 18:37:05 -0400

    added vue

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+      <h1>tango</h1>
+    <!-- <img src="./assets/logo.png"> -->
+    <HelloWorld/>
+    <tango/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+import tango from './components/tango'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    tango
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/tango.vue
@@ -0,0 +1,37 @@
+<template>
+    <span>yo big brass {{object}}</span>
+</template>
+
+
+
+<script>
+export default {
+  object: 'tango',
+  data () {
+    return {
+      object: 'donkadonk'
+    }
+  }
+}
+</script>
+
+
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+  color: aquamarine;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
